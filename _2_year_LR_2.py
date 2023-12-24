756e0a1204ef872f61bf6217601012cbabb52406
PertsevK <133851085+PertsevK@users.noreply.github.com>
2023-12-24 22:19:04 +0300

    Update _2_year_LR_2.py

--- _2_year_LR_2.py
+++ _2_year_LR_2.py
@@ -4,44 +4,46 @@ from math import factorial
 def task_if6():
     """Two numbers are given. Bring out more of them"""
     try: # Перевірка
-        num1=float(input("Enter a Number: ")) # Введення першого числа
-        num2=float(input("Enter a Number: ")) # Введення другого числа
-        if num1>=num2: # Порівняння
+        num1 = float(input("Enter a Number: "))  # Введення першого числа
+        num2 = float(input("Enter a Number: "))  # Введення другого числа
+        if num1 >= num2:  # Порівняння
             print("Answer: ", num1)
         else:
             print("Answer: ", num2)    
-    except: # Якщо помилка
+    except ValueError: # Якщо помилка
         print("FLOAT expected!")
         
 def task_geom19():
-    """Check the series (variant 19) for convergence"""
-    try: # Перевірка
-        r=float(input("Enter a Radius: ")) # Введення радіуса
-        if r>0:
-            x=float(input("Enter x: ")) # Введення координат
-            y=float(input("Enter y: "))
-            if x>=0 and x<=r and y>=x-2*r and (x-r)*(x-r)+(y-r)*(y-r)>=r:
-                print("The point is in the area") # Якщо входить в область
-            else: print("The point is not in the area") # Якщо не входить
-        else: print("r must be positive")
-    except: # Якщо помилка
+    """Geometry 19"""
+    try:  # Перевірка
+        r = float(input("Enter a Radius: "))  # Введення радіуса
+        if r < 0:
+            raise ValueError
+        for i in range(int(input("Enter point amount: "))):
+            x = float(input(f"Enter x{i+1}: "))  # Введення координат
+            y = float(input(f"Enter y{i+1}: "))
+            if x >= 0 and x <= r and y >= x - 2 * r and (x - r) * (x - r) + (y - r) * (y - r) >= r:
+                print(f"The point {i+1} is in the area")  # Якщо входить в область
+            else:
+                print(f"The point {i+1} is not in the area")  # Якщо не входить
+    except ValueError:  # Якщо помилка
         print("FLOAT expected!")
         
 def task_series10():
     """Check the series (variant 10) for convergence"""
-    n = 1 # Початкове значення n
-    s = u = 2.0 # Значення ряду в точці n=1
-    e = 1e-10 # g = 1e+10 - точність
-    while abs(u) > e: #abs(u) < g
+    n = 1  # Початкове значення n
+    s = u = 2.0  # Значення ряду в точці n=1
+    e = 1e-10  # g = 1e+10 - точність
+    while abs(u) > e:  # abs(u) < g
         print(u)
         n += 1
-        if ((n/2)**n) == 0:
-            break
-        u = (factorial(n)-3**n)/n**n # Формула
-        s += u
+        try:
+            u = (factorial(n) - 3 ** n) / n ** n  # Формула
+        except ZeroDivisionError:
+            print("Division by zero!")
+            return False
+        else:
+            s += u
     else:
-        print ("Series converge to: ",s) #"Maximum sum is:"
+        print("Series converge to: ", s)  # "Maximum sum is:"
         return True
-    print("Division by zero!")
-    return False
-
